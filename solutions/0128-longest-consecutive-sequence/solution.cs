833670da3a0c7170fb09a03e24f08858e3e5c57e
Max Veerhoek <lmg@veerhoek.eu>
2024-11-01 15:25:04 +0000

    [LeetCode Sync] Runtime - 12 ms (90.64%), Memory - 66.5 MB (60.82%)

---
+++ solutions/0128-longest-consecutive-sequence/solution.cs
@@ -0,0 +1,29 @@
+using System.Globalization;
+
+public class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var numbersSet = new HashSet<int>(nums);
+        var longestSequenceLength = 0;
+
+        foreach (var num in numbersSet)
+        {
+            if (!numbersSet.Contains(num - 1))
+            {
+                var currentSequenceLength = 1;
+
+                var nextNumber = num + 1;
+                while (numbersSet.Contains(nextNumber)) {
+                    currentSequenceLength++;
+                    nextNumber++;
+                }
+
+                longestSequenceLength = Math.Max(longestSequenceLength, currentSequenceLength);
+            }
+
+        }
+
+        return longestSequenceLength;
+    }
+}
