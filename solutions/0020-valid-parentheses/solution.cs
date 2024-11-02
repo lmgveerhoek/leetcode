02362b64a4723183b0dac585f19f0d86ceb6dd19
Max Veerhoek <lmg@veerhoek.eu>
2024-11-02 08:56:18 +0000

    [LeetCode Sync] Runtime - 3 ms (50.38%), Memory - 40.9 MB (11.08%)

---
+++ solutions/0020-valid-parentheses/solution.cs
@@ -0,0 +1,31 @@
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        // Bases case
+        if (s.Length == 1)
+            return false;
+
+        var bracketPairs = new Dictionary<char, char> {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' }
+        };
+
+        // Create a Queue, to keep track of the brackets
+        var parenthesesStack = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            // Add to stack if it's an opening bracket
+            if (bracketPairs.ContainsKey(c))
+                parenthesesStack.Push(c);
+            // Check if there are brackets to close with or the stack is empty
+            else if (parenthesesStack.Count == 0 || bracketPairs[parenthesesStack.Pop()] != c)
+                return false;
+        }
+
+        return parenthesesStack.Count == 0;
+    }
+}
